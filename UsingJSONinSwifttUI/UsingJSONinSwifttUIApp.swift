a3e6454611246cb409a1c3beeb6dc142642f74de
Sandy Gomez <sgomez1029@gmail.com>
2024-04-18 21:34:00 -0700

    Initial Commit

---
+++ UsingJSONinSwifttUI/UsingJSONinSwifttUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsingJSONinSwifttUIApp.swift
+//  UsingJSONinSwifttUI
+//
+//  Created by Sandra Gomez on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct UsingJSONinSwifttUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
