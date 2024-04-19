f15115b9c146137135db56770e1a2b6ff732bdc8
Sandy Gomez <sgomez1029@gmail.com>
2024-04-18 21:43:20 -0700

    Using JSON in SwiftUI

--- UsingJSONinSwifttUI/ContentView.swift
+++ UsingJSONinSwifttUI/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var message = ""
+    let jsonString = """
+    {
+        "name": "Frank",
+        "age": 48,
+        "gender": "male",
+    }
+    """
+    
+    // This structure will hold the JSON Data that consists of a name(String), age (Int), and gender (String).
+    struct Employee: Codable {
+        var name: String
+        var age: Int
+        var gender: String
+    }
+    
+    
+    
     var body: some View {
+            let jsonStuff = jsonString.data(using: .utf8)
+            let decoder = JSONDecoder()
+            let person = try! decoder.decode(Employee.self, from: jsonStuff!)
+            let message = "\(person.name) is a \(person.age) year old \(person.gender)"
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(message)
         }
-        .padding()
     }
 }
 
